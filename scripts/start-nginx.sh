f3d5889dcd2340ada1f2cdd9fc4dc44281ea879f
Hector <hector.morales.carnice@gmail.com>
2023-05-01 01:54:24 +0200

    fix: update script for multipage application

--- scripts/start-nginx.sh
+++ scripts/start-nginx.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
-export JS_TARGET=/etc/static/js/app.*.js;
-
-cat /etc/static/index.html | envsubst $EXISTING_VARS | tee /etc/static/index.html 
+export JS_TARGET=/etc/static/js/*.*.js;
 
 for file in $JS_TARGET; do
-    cat $file | envsubst $EXISTING_VARS | tee $file
+    cat $file | envsubst $EXISTING_VARS | tee $file.out
+    mv --force $file.out $file
 done
 
+cat /etc/static/index.html | envsubst $EXISTING_VARS | tee /etc/static/index.html 
 nginx -g 'daemon off;'
